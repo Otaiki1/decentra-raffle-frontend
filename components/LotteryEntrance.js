9564aa9cb79b9412f8d435b3c9abb21a1fd2cbc3
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-24 15:35:51 +0100

    refac: setup fetch current winner

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,15 +1,43 @@
-import { useWeb3Contract } from "react-moralis";
-import abi from "../constants/abi.json"
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import abi from "../constants/abi.json";
+import { useState, useEffect } from "react";
+
+const CONTRACT_ADDRESS = "0xa42FaE009b4656a7DD32a59033aac0999B2b466f"
 
 export default function LotteryEntrance(){
+
+    const[recentWinner, setRecentWinner] = useState("0");
+    const[ numPlayers, setNumPlayers] = useState("0");
+    const{ isWeb3Enabled } = useMoralis();
+
     //Enter lottery Button Function
     const { runContractFunction: enterRaffle } = useWeb3Contract({
         abi: abi,
-        contractAddress: "0xa42FaE009b4656a7DD32a59033aac0999B2b466f",
+        contractAddress: CONTRACT_ADDRESS,
         functionName: "enterRaffle",
         msgValue: "100000000000000000",//0.1 eth
         params: {}
     })
+    
+    // ViewFunction
+    const { runContractFunction: getRecentWinner} = useWeb3Contract({
+        abi: abi,
+        contractAddress: CONTRACT_ADDRESS,
+        functionName: "s_recentWinner",
+        params: {},
+    })
+
+    useEffect(() => {
+
+        async function updateUI(){
+            const recentWinnerFromCall = await getRecentWinner();
+            console.log("The recent winner from call is___---", recentWinnerFromCall)
+            setRecentWinner(recentWinnerFromCall);
+        }
+        if(isWeb3Enabled){
+            updateUI()
+        }
+    }, [isWeb3Enabled]);
 
     return(
         <div>
@@ -18,6 +46,7 @@ export default function LotteryEntrance(){
             }}>
                 Enter Lottery
             </button>
+            <p>The recent winner is :{recentWinner}</p>
         </div>
     )
 }
