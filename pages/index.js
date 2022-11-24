cd1a09a98ecd93b25efb8e91c9be395cd29586f8
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-24 11:17:58 +0100

    refac: add connectWallet detecting function

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
 import LotteryEntrance from '../components/LotteryEntrance';
+import { useMoralis } from 'react-moralis';
 
 export default function Home() {
+
+  const { isWeb3Enabled } = useMoralis();
+
   return (
     <div className={styles.container}>
       <Header />
-     <LotteryEntrance />
+      {isWeb3Enabled ? 
+          <LotteryEntrance />
+        :
+          <div>
+            No MetaMask Detected!!
+          </div>
+      }
+      
     </div>
   )
 }
